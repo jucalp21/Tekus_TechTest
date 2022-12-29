04926941255354db2801312a3d1a8d9542780cb0
Juan Camilo Loaiza P <juancalope21@hotmail.com>
2022-12-29 16:05:31 -0500

    [Commit] Header General Component added to project

---
+++ src/app/views/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { RouterLink, RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterLink
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class ComponentsModule { }
